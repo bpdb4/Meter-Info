fbc80544a90ab8f659e1534ac4791a7936eed5b4
bpdb4 <ujasim2014@gmail.com>
2025-11-15 22:13:44 +0600

    Create HtmlActivity.java

---
+++ Meter Info/app/src/main/java/customerinfo/app/HtmlActivity.java
@@ -0,0 +1,49 @@
+package customerinfo.app;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HtmlActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.htmlactivity);
+        
+        webView = findViewById(R.id.webView);
+        setupWebView();
+    }
+
+    private void setupWebView() {
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.addJavascriptInterface(new WebAppInterface(), "AndroidInterface");
+        webView.loadUrl("file:///android_asset/application_form.html");
+    }
+
+    public class WebAppInterface {
+        @android.webkit.JavascriptInterface
+        public String fetchDataForApplication(String inputNumber, String type) {
+            try {
+                ApplicationFormHelper helper = new ApplicationFormHelper(HtmlActivity.this);
+                java.util.Map<String, Object> result = helper.fetchDataForApplicationForm(inputNumber, type);
+                org.json.JSONObject jsonResult = new org.json.JSONObject(result);
+                return jsonResult.toString();
+            } catch (Exception e) {
+                return "{\"error\":\"Data fetch failed: " + e.getMessage() + "\"}";
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
